b0a9eb63a557753d3e5aae08680584c420a2ccd5
Michele Fenu <michelefenu@gmail.com>
2020-09-14 14:55:14 +0000

    First draft

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as json2interface from 'json2interface';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,15 +14,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('json2typescript.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('json2typescript.generate', () => {
+		const editor = vscode.window.activeTextEditor;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from json2typescript!');
+		try {
+			vscode.env.clipboard.readText().then(value => editor?.edit(editBuilder => {
+					editBuilder.insert(editor.selection.active, json2interface.generate(value));
+				}));
+
+			vscode.window.showInformationMessage('Valid JSON');
+		} catch (exception) {
+			vscode.window.showErrorMessage('The clipboard does not contains a valid JSON');
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
