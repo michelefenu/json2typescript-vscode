f1774c294d7355811c8d454160656841eb17c324
Michele Fenu <michelefenu@gmail.com>
2020-09-15 16:33:42 +0200

    Small refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -4,41 +4,45 @@ import { generate } from 'json2interface'
 export function activate (context: vscode.ExtensionContext) {
   const disposable = vscode.commands.registerCommand(
     'json2typescript.generate',
-    () => {
-      vscode.env.clipboard.readText().then(value => {
-        try {
-          const typeScriptInterfaces = generate(value)
-
-          const activeTextEditor = vscode.window.activeTextEditor
-
-          if (!activeTextEditor) {
-            vscode.workspace
-              .openTextDocument({
-                language: 'typescript',
-                content: typeScriptInterfaces
-              })
-              .then(document => {
-                vscode.window.showTextDocument(document)
-              })
-          } else {
-            activeTextEditor.edit(edit => {
-              const selection = activeTextEditor.selection.active
-              edit.insert(selection, typeScriptInterfaces)
-            })
-          }
-
-          vscode.window.showInformationMessage(
-            'TypeScript interfaces generated'
-          )
-        } catch (exception) {
-          vscode.window.showErrorMessage(
-            'The clipboard does not contains a valid JSON'
-          )
-        }
-      })
-    }
+    () => generateInterfaces()
   )
   context.subscriptions.push(disposable)
+}
+
+async function generateInterfaces () {
+  const clipBoardContent = await readFromClipboard()
+
+  try {
+    const typeScriptInterfaces = generate(clipBoardContent)
+
+    const activeTextEditor = vscode.window.activeTextEditor
+
+    if (!activeTextEditor) {
+      vscode.workspace
+        .openTextDocument({
+          language: 'typescript',
+          content: typeScriptInterfaces
+        })
+        .then(document => {
+          vscode.window.showTextDocument(document)
+        })
+    } else {
+      activeTextEditor.edit(edit => {
+        const selection = activeTextEditor.selection.active
+        edit.insert(selection, typeScriptInterfaces)
+      })
+    }
+
+    vscode.window.showInformationMessage('TypeScript interfaces generated')
+  } catch (exception) {
+    vscode.window.showErrorMessage(
+      'The clipboard does not contain a valid JSON'
+    )
+  }
+}
+
+async function readFromClipboard () {
+  return vscode.env.clipboard.readText()
 }
 
 // this method is called when your extension is deactivated
